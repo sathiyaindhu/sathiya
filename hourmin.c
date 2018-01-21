712d7715f994ae10e0e0789fd4a455632f2f594b
sathiyaindhu <31506120+sathiyaindhu@users.noreply.github.com>
2018-01-21 12:15:08 +0530

    Create hourmin.c

---
+++ hourmin.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int min,hour,total_minutes;
+printf("enter the hours");
+scanf("%d",&hour);
+printf("enter the minutes");
+scanf("%d",&min);
+total_minutes=(hours*60)+minutes;
+printf("hours and minutes are");
+return 0;
+}
