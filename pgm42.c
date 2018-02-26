96ab4fb89ba884285ddea33b117400700333ebab
sathiyaindhu <31506120+sathiyaindhu@users.noreply.github.com>
2018-02-26 15:51:48 +0530

    Create pgm42.c

---
+++ pgm42.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    char s1[20],s2[20];
+    int i,j;
+    scanf("%s",&s1);
+    scanf("%s",&s2);
+    if(s1[i]==s2[j])
+    {
+        printf("%s",s1);
+    }
+else if(s1[i]>s2[i])
+{
+    printf("%s",s1);
+}
+else
+{
+    printf("%s",s2);
+}
+}
