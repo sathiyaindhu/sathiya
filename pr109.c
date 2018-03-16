e18779d18ba1c9a567d5f15c291630afe319b700
sathiyaindhu <31506120+sathiyaindhu@users.noreply.github.com>
2018-03-16 16:04:38 +0530

    Create pr109.c

---
+++ pr109.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int a[10],i,j,t;
+	for(i=0;i<10;i++)
+	{
+	scanf("%d",&a[i]);
+	}
+	for(i=0;i<10;i++)
+	{
+	    for(j=i+1;j<10;j++)
+	    {
+	        if(a[i]>a[j])
+	        {
+	            t=a[i];
+	            a[i]=a[j];
+	            a[j]=t;
+	        }
+	    }
+	}
+	printf("%d",a[0]);
+  return 0;
+}
